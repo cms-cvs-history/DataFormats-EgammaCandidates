1b0c44e352b35e47e8e3dac63652ae23f53b97f3
cmsbuild <>
2007-01-26 16:24:43 +0000

    This commit was manufactured by cvs2git to create tag 'DataFormats- EgammaCandidates-V00-03-00-01'.
    Sprout from master 2007-01-17 10:23:29 UTC Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch> 'added virtual member function pdgId()'
    Cherrypick from master 2007-01-26 16:24:39 UTC Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch> 'Replace reco::TrackRefVector with  std::vector<reco::TrackRef>':
        interface/ConvertedPhoton.h
        src/ConvertedPhoton.cc

--- interface/ConvertedPhoton.h
+++ interface/ConvertedPhoton.h
@@ -6,7 +6,7 @@
  *
  * \author N.Marinelli  University of Notre Dame, US
  *
- * \version $Id: ConvertedPhoton.h,v 1.1 2006/06/22 12:41:33 nancy Exp $
+ * \version $Id: ConvertedPhoton.h,v 1.2 2006/12/19 18:12:53 nancy Exp $
  *
  */
 #include "DataFormats/RecoCandidate/interface/RecoCandidate.h"
@@ -15,21 +15,13 @@
 #include "DataFormats/EgammaReco/interface/SuperCluster.h"
 
 namespace reco {
-
-  class ConvertedPhoton : public RecoCandidate {
+    class ConvertedPhoton : public RecoCandidate {
   public:
     /// default constructor
     ConvertedPhoton() : RecoCandidate() { }
-    /// constructor from values
-    ConvertedPhoton( Charge q, const LorentzVector & p4, const Point & vtx = Point( 0, 0, 0 ) ) : 
-      RecoCandidate( q, p4, vtx ) { }
-    // 
-    ConvertedPhoton( const reco::SuperClusterRef scl,  const reco::TrackRefVector trkRefs , Charge q, const LorentzVector & p4, const Point & vtx = Point( 0, 0, 0 ) ):   RecoCandidate( q, p4, vtx ), superCluster_(scl), tracks_(trkRefs)  { }
-    //
 
-    ConvertedPhoton( const reco::SuperClusterRef scl,  Charge q, const LorentzVector & p4, const Point & vtx = Point( 0, 0, 0 ) ):   RecoCandidate( q, p4, vtx ), superCluster_(scl) { }
+   ConvertedPhoton( Charge q, const LorentzVector & p4, const Point & vtx = Point( 0, 0, 0 ), const Point & convVtx = Point( 0, 0, 0 ) ):  RecoCandidate( q, p4, vtx ), theConversionVertex_(convVtx) { }
 
-   
     /// destructor
     virtual ~ConvertedPhoton();
     /// returns a clone of the candidate
@@ -38,15 +30,32 @@ namespace reco {
     reco::SuperClusterRef superCluster() const ;
     /// set refrence to ConvertedPhoton component
     void setSuperCluster( const reco::SuperClusterRef & r ) { superCluster_ = r; }
+
+
+    // set reference to a pair of Tracks
+    //    void setTrackPairRef( const reco::TrackRefVector & pair ) { tracks_ = pair; }
+    void setTrackPairRef( const  std::vector<reco::TrackRef>  & pair ) { tracks_ = pair; }
     // reference to a vector of tracks
-    reco::TrackRefVector tracks() const ;  
+    // reco::TrackRefVector tracks() const ;  
+    std::vector<reco::TrackRef> tracks() const ; 
+    // returns the position of the conversion vertex
+
+
+
+
+    const Point & convVertexPosition() const { return theConversionVertex_ ; }
 
   private:
     /// check overlap with another candidate
     virtual bool overlap( const Candidate & ) const;
+   
+
     /// reference to a SuperCluster
     reco::SuperClusterRef superCluster_;
-    reco::TrackRefVector  tracks_;
+    //    reco::TrackRefVector  tracks_;
+    std::vector<reco::TrackRef>  tracks_;
+    reco::Particle::Point theConversionVertex_;
+
   };
   
 }
